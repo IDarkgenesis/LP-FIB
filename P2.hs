c08d1afa82db8fe3c7aab2f9c3d0dcb23d496e5f
Bartomeu Perelló <byidarkgenesis@gmail.com>
2020-02-16 23:07:56 +0100

    P2 menys darrer

--- P2.hs
+++ P2.hs
@@ -17,6 +17,52 @@ myMaximum xs = myMaximum' (tail xs) (head xs)
             | (head xa) > m = myMaximum' (tail xa) (head xa)
             | (head xa) <= m = myMaximum' (tail xa) m
             
+            
 --FUNCIO MITJANA
 
 average :: [Int] -> Float
+
+average xs = fromIntegral (div (mySum xs) (myLength xs))
+    where 
+        mySum::[Int] -> Int
+        mySum  xs 
+            | null xs = 0
+            | otherwise = (head xs) + mySum (tail xs)
+            
+--FUNCIO CREATE PALINDROME
+
+buildPalindrome :: [Int] -> [Int]
+buildPalindrome xs= concat [reverse xs, xs] 
+
+
+--FUNCIO REMOVE
+{-
+remove :: [Int] -> [Int] -> [Int]
+remove xo xe = remove' xo xe []
+    where
+        remove' :: [Int] -> [Int] -> [Int] -> [Int]
+        remove' xs xa r
+            | null xs = remove' r (tail xa) []
+            | null xa = r
+            | (head xs) == (head xa) = remove' (tail xs) xa r
+            | otherwise = remove' (tail xs) xa (r ++ [head xs])
+            -}
+            
+remove :: [Int] -> [Int] -> [Int]
+remove [] _ = []
+remove (x:xs) xa
+    | elem x xa= remove xs xa
+    | otherwise = x : remove xs xa
+    
+--FUNCIO FLATTEN
+flatten :: [[Int]] -> [Int]
+flatten [] = []
+flatten (x:xs)= x ++ flatten xs
+
+--FUNCIO ODD EVENS
+oddsNevens :: [Int] -> ([Int],[Int])
+oddsNevens [] = ([],[])
+oddsNevens (x:xs)
+    | even x = (od , x : ev)
+    | otherwise = (x : od, ev)
+    where (od,ev) = oddsNevens(xs)
