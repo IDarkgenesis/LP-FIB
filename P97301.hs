13c5425336002ddada155a8984fa7725e31b09a4
Bartomeu Perello Comas <bartomeu.perello@c6s307mac08.fib.upc.es>
2020-03-09 12:29:55 +0100

    Fizz

--- P97301.hs
+++ P97301.hs
@@ -1,7 +1,13 @@
+getAct :: Int -> Either Int String
+
+getAct x
+    | mod(x,3) == 0 && mod(x,5) == 0 = Right ("FizzBuzz")
+    | mod(x,3) == 0 = Right("Fizz")
+    | mod(x,5) == 0 = Right("Buzz")
+    | otherwise = Left(x)
+    
 fizzBuzz :: [Either Int String]
+
 fizzBuzz = fizzBuzz' (iterate (+1) 0)
     where 
-        fizzBuzz' (x:xs) = aux : fizzBuzz [next | next <- xs]
-    where 
-        
-    
+        fizzBuzz' (x:xs) = getAct x : fizzBuzz' [next | next <- xs]
