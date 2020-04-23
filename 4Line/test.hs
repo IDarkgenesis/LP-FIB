f8f30f7ceb1c6ea5bc573270c3a5b8cbfd1fad31
Bartomeu Perelló <byidarkgenesis@gmail.com>
2020-04-23 11:23:44 +0200

    Acaba :D

--- 4Line/test.hs
+++ 4Line/test.hs
@@ -1,3 +1,9 @@
 data Cat = Cat {fs :: String,
                 ls :: String
                 } deriving (Show)
+
+checkLen :: [Int] -> Bool
+checkLen chk = case len of
+                    3  -> True
+                where
+                    len= length chk
