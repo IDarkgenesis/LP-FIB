4f3df348d39e4b49cd663f433f1b13398165ff17
Bartomeu Perelló <byidarkgenesis@gmail.com>
2020-04-23 20:46:08 +0200

    Diagonal F -bug canvi de direccio-

--- 4Line/4Line.hs
+++ 4Line/4Line.hs
@@ -36,6 +36,9 @@ main = do
 data Strats= Human | Rng | Greedy | Smart
     deriving (Eq)
 
+data Direction= Up | Down 
+    deriving (Eq)
+    
 data Player = Player {  pid :: Int, 
                         strat :: Strats}
 
@@ -264,6 +267,13 @@ winV c cs m
             | (length consec) > 0 && (head consec) == (head elms) = checkWinner (tail elms) ((head elms):consec)
             | otherwise = checkWinner (tail elms)  [(head elms)]
 
+            
+getPieceInt :: Int -> [Int] -> Int
+getPieceInt r xs
+    | (r - length(xs)) < 0  = getPieceInt r (tail xs)
+    | (r - length(xs)) > 0  = 0
+    | (r - length(xs)) == 0 = (head xs)            
+            
 {- winH -> 
 -}
 winH :: Int -> Int -> Map Int [Int] -> Int
@@ -299,14 +309,72 @@ winH r cs m
                match = (obj == (head consec))
                isZero = (obj == 0)
                obj = getPieceInt r (fromMaybe [] (find c m))
-               getPieceInt :: Int -> [Int] -> Int
-               getPieceInt r xs
-                   | (r - length(xs)) < 0  = getPieceInt r (tail xs)
-                   | (r - length(xs)) > 0  = 0
-                   | (r - length(xs)) == 0 = (head xs)  
-               
+
+--QUANT ESTA A NES BORDE COLUMNA NECESITA GUARDAR VALOR NOU CAS PER EXEMPLE R-1 QUE NECESSITA OBJ QUANT CANVI DE DIRECCIO                 
+winDF :: Int -> Int -> Int -> Int -> Direction -> [Int] -> Map Int [Int] -> Int
+winDF r c rs cs dir consec m
+    | r == 1 && c == cs = 0
+    | dir == Up && (c+1) > cs = case lenConsec of
+                                     3 -> case match of
+                                               True  -> obj
+                                               False -> winDF (r-1) c rs cs Down [] m
+                                     n -> winDF (r-1) c rs cs Down [] m
+    
+    | dir == Up && (r+1) > rs =  case lenConsec of
+                                     3 -> case match of
+                                               True  -> obj
+                                               False -> winDF r (c+1) rs cs Down [] m
+                                     n -> winDF r (c+1) rs cs Down [] m
+    
+    | dir == Down && (r-1) < 1 = case lenConsec of
+                                      3 -> case match of
+                                                True  -> obj
+                                                False -> winDF r (c+1) rs cs Up [] m
+                                      n -> winDF r (c+1) rs cs Up [] m
+
+    | dir == Down && (c-1) < 1 = case lenConsec of
+                                      3 -> case match of
+                                                True  -> obj
+                                                False -> winDF (r-1) c rs cs Up [] m
+                                      n -> winDF r (c+1) rs cs Up [] m
+    
+    | dir == Up = case lenConsec of
+                       3 -> case match of
+                                 True  -> obj
+                                 False -> case isZero of
+                                               True  -> winDF (r+1) (c+1) rs cs Up [] m
+                                               False -> winDF (r+1) (c+1) rs cs Up [obj] m
+                       0 -> case isZero of
+                                 True  -> winDF (r+1) (c+1) rs cs Up [] m
+                                 False -> winDF (r+1) (c+1) rs cs Up [obj] m
+                       n -> case match of
+                                 True  -> winDF (r+1) (c+1) rs cs Up (obj:consec) m
+                                 False -> case isZero of
+                                               True  -> winDF (r+1) (c+1) rs cs Up [] m
+                                               False -> winDF (r+1) (c+1) rs cs Up [obj] m
+                                               
+     | dir == Down = case lenConsec of
+                       3 -> case match of
+                                 True  -> obj
+                                 False -> case isZero of
+                                               True  -> winDF (r-1) (c-1) rs cs Down [] m
+                                               False -> winDF (r-1) (c-1) rs cs Down [obj] m
+                       0 -> case isZero of
+                                 True  -> winDF (r-1) (c-1) rs cs Down [] m
+                                 False -> winDF (r-1) (c-1) rs cs Down [obj] m
+                       n -> case match of
+                                 True  -> winDF (r-1) (c-1) rs cs Down (obj:consec) m
+                                 False -> case isZero of
+                                               True  -> winDF (r-1) (c-1) rs cs Down [] m
+                                               False -> winDF (r-1) (c-1) rs cs Down [obj] m
+     where
+         lenConsec = length consec
+         match = (obj == (head consec))
+         isZero = (obj == 0)
+         obj = getPieceInt r (fromMaybe [] (find c m))
+                                      
 anyWin :: Int -> Int -> Map Int [Int] -> Int
-anyWin r c m = (winV 1 c m) + (winH r c m)
+anyWin r c m = (winV 1 c m) + (winH r c m) + (winDF r c r c Up [] m)
 
 {- initGame -> Inicialitza el joc amb el tauler corresponent a part de crear els         jugardors pertinents
     
